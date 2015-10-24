e401cf78adf1b1d66557258ed7e0714e72c4c7fb
pundliksarafdar <sarafdarpundlik@gmail.com>
2015-10-25 00:21:10 +0530

    Logger added

--- ClassDatabase/src/main/java/com/classapp/db/register/RegisterUser.java
+++ ClassDatabase/src/main/java/com/classapp/db/register/RegisterUser.java
@@ -7,9 +7,11 @@ import com.classapp.persistence.Constants;
 import com.classapp.persistence.HibernateUtil;
 import com.classapp.servicetable.ServiceMap;
 import com.google.gson.Gson;
+import com.classapp.logger.AppLogger;
 
 public class RegisterUser {
 	public String registerUser(String registerRequest){
+			AppLogger.logger("");
 			final String success = "success";
 			Gson gson = new Gson();
 			RegisterBean registerBean = gson.fromJson(registerRequest, RegisterBean.class);
@@ -23,6 +25,7 @@ public class RegisterUser {
 				session.save(registerBeantoSave);
 				session.getTransaction().commit();
 			}catch(Exception e){
+				e.printStackTrace();
 				session.getTransaction().rollback();
 				errorMessage = formatMessage(e.getCause().getMessage());
 				return errorMessage;
