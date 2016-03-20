14a7e48ff5be1e627f9fc361ee2b1abdc785bf83
pundliksarafdar <sarafdarpundlik@gmail.com>
2016-03-21 00:56:30 +0530

    checkin after inversion

--- ClassDatabase/src/main/java/com/classapp/db/institutestats/InstituteStatsDB.java
+++ ClassDatabase/src/main/java/com/classapp/db/institutestats/InstituteStatsDB.java
@@ -1,5 +1,9 @@
 package com.classapp.db.institutestats;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -147,5 +151,48 @@ public class InstituteStatsDB {
 		return  stats;
 	}
 	
+	public List<InstituteStats> getAllInstStats() {
+		List<InstituteStats> stats=new ArrayList<InstituteStats>();
+		Transaction transaction=null;
+		Session session=null;
+		session=HibernateUtil.getSessionfactory().openSession();
+		transaction=session.beginTransaction();
+		Query query = session.createQuery("select a,b.className from InstituteStats a,RegisterBean b where a.inst_id = b.regId");
+		List list = query.list();
+		for(Object object:list){
+			Object[] objects = (Object[]) object;
+			InstituteStats instituteStats = (InstituteStats) objects[0];
+			String sClassname = (String) objects[1];
+			instituteStats.setClassname(sClassname);
+			stats.add(instituteStats);
+		}
+		transaction.commit();
+		session.close();
+		return  stats;
+	}
 	
+	public boolean updateLimit(InstituteStats instituteStats) {
+		InstituteStats stats=new InstituteStats();
+		stats.setInst_id(instituteStats.getInst_id());
+		Transaction transaction=null;
+		Session session=null;
+		session=HibernateUtil.getSessionfactory().openSession();
+		transaction=session.beginTransaction();
+		stats=(InstituteStats) session.get(InstituteStats.class,instituteStats.getInst_id());
+		
+		stats.setAlloc_ids(instituteStats.getAlloc_ids());
+		stats.setAlloc_memory(instituteStats.getAlloc_memory());
+		
+		stats.setSmsAccess(instituteStats.isSmsAccess());
+		stats.setSmsAlloted(instituteStats.getSmsAlloted());
+		
+		stats.setEmailAccess(instituteStats.isEmailAccess());
+		
+		session.saveOrUpdate(stats);
+		transaction.commit();
+		if(null!=session){
+			session.close();
+		}
+		return  true;
+	}
 }
