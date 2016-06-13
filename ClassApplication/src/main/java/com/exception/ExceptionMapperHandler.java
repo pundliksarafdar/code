6e4657a6160be7f1e9c5d0775a8e95ca1a831484
pundliksarafdar <sarafdarpundlik@gmail.com>
2016-06-14 00:01:33 +0530

    Exception listener

--- ClassApplication/src/main/java/com/exception/ExceptionMapperHandler.java
+++ ClassApplication/src/main/java/com/exception/ExceptionMapperHandler.java
@@ -1,5 +1,12 @@
 package com.exception;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -8,14 +15,37 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class ExceptionMapperHandler implements ExceptionMapper<NoUserBeanException>{
-
+	private static Properties EXCEPTION_MAP;
+	
 	@Override
 	public Response toResponse(NoUserBeanException exception) {
+		if(null==EXCEPTION_MAP || EXCEPTION_MAP.isEmpty()){
+			EXCEPTION_MAP = getExceptionProperties();
+		}
+		
+		
 		// TODO Auto-generated method stub
 		ExceptionBean exceptionBean = new ExceptionBean();
-		exceptionBean.setCode("NO_USER_BEAN");
-		exceptionBean.setMessage(exception.getMessage());
+		exceptionBean.setCode(exception.getMessage());
+		exceptionBean.setMessage(EXCEPTION_MAP.getProperty(exception.getMessage()));
 		return Response.status(Status.BAD_REQUEST).entity(exceptionBean).type(MediaType.APPLICATION_JSON).build();
+	}
+	
+	private Properties getExceptionProperties(){
+		InputStream fileInput;
+		Properties properties = new Properties();
+		try {
+			fileInput = getClass().getClassLoader().getResourceAsStream("exception.properties");
+			properties.load(fileInput);
+			fileInput.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return properties;
 	}
 
 }
